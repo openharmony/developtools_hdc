ed18e8efc273ff7c5b400be6cdf6a4b18d255501
liupeng298 <liupeng298@huawei.com>
2024-07-26 08:19:32 +0000

    update src/common/transfer.cpp.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- src/common/transfer.cpp
+++ src/common/transfer.cpp
@@ -199,6 +199,19 @@ out:
     return ret;
 }
 
+void HdcTransferBase::Next(HdcTransferBase *thisClass, CtxFile *context)
+{
+    if (context->isDir && context->master) {
+        uint8_t payload = 1;
+        thisClass->CommandDispatch(CMD_FILE_FINISH, &payload, 1);
+    } else if (context->isDir && !context->master) {
+        uint8_t payload = 1;
+        thisClass->SendToAnother(CMD_FILE_FINISH, &payload, 1);
+    } else {
+        thisClass->TaskFinish();
+    }
+}
+
 void HdcTransferBase::OnFileIO(uv_fs_t *req)
 {
     CtxFileIO *contextIO = reinterpret_cast<CtxFileIO *>(req->data);
@@ -232,16 +245,8 @@ void HdcTransferBase::OnFileIO(uv_fs_t *req)
                 WRITE_LOG(LOG_DEBUG, "OnFileIO read size 0, isDir:%d, master:%d",
                     context->isDir, context->master);
                 thisClass->LogMsg(MSG_FAIL, "Error reading file: read nothing, path:%s",
-                          context->localPath.c_str());    
-                if (context->isDir && context->master) {
-                    uint8_t payload = 1;
-                    thisClass->CommandDispatch(CMD_FILE_FINISH, &payload, 1);
-                } else if (context->isDir && !context->master) {
-                    uint8_t payload = 1;
-                    thisClass->SendToAnother(CMD_FILE_FINISH, &payload, 1);
-                } else {
-                    thisClass->TaskFinish();
-                }
+                    context->localPath.c_str());
+                Next(thisClass, context);
                 return;
             }
             if (!thisClass->SendIOPayload(context, context->indexIO - req->result, bufIO, req->result)) {

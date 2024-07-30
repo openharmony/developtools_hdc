0870d74ce1253b7b16016aa3c54f5639d34d36cd
liupeng298 <liupeng298@huawei.com>
2024-07-30 02:39:56 +0000

    update test/fuzztest/forwardreadstream_fuzzer/ForwardReadStream_fuzzer.cpp.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- test/fuzztest/forwardreadstream_fuzzer/ForwardReadStream_fuzzer.cpp
+++ test/fuzztest/forwardreadstream_fuzzer/ForwardReadStream_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -47,8 +47,13 @@ bool FuzzForwardReadStream(const uint8_t *data, size_t size)
     uv_pipe_t pipe;
     pipe.data = ctx;
     uv_stream_t *stream = (uv_stream_t *)&pipe;
-    uv_buf_t rbf = uv_buf_init(reinterpret_cast<char *>(const_cast<uint8_t *>(data)), size);
-    forward->ReadForwardBuf(stream, (ssize_t)size, &rbf);
+    uv_buf_t *rbf = (uv_buf_t*)malloc(sizeof(uv_buf_t));
+    if (rbf == nullptr) {
+        return false;
+    }
+    rbf->base = new char[size];
+    (void)memcpy_s(rbf->base, size, reinterpret_cast<char *>(const_cast<uint8_t *>(data)), size);
+    forward->ReadForwardBuf(stream, (ssize_t)size, rbf);
     delete ctx;
     delete hTaskInfo;
     delete daemon;

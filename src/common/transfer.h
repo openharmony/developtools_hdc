3c91b030d28cb2b3632f1bc8cd4bc428124c82b5
liupeng298 <liupeng298@huawei.com>
2024-12-03 01:51:03 +0000

    update src/common/transfer.h.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- src/common/transfer.h
+++ src/common/transfer.h
@@ -137,7 +137,7 @@ protected:
     void ExtractRelativePath(string &cwd, string &path);
     bool AddFeatures(FeatureFlagsUnion &feature);
     bool CheckFeatures(CtxFile *context, uint8_t *payload, const int payloadSize);
-    void RemoveSandboxRootPath(std::string &errStr, std::string &bundleName);
+    void RemoveSandboxRootPath(std::string &srcStr, std::string &bundleName);
     bool IsValidBundleName(string &bundleName);
     void CloseCtxFd(CtxFile *context);
 

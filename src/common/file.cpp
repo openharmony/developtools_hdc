6d5b5cbd864c5cdad37766087139cfe411d1c20d
liupeng298 <liupeng298@huawei.com>
2024-12-13 08:09:08 +0000

    update src/common/file.cpp.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- src/common/file.cpp
+++ src/common/file.cpp
@@ -151,6 +151,7 @@ bool HdcFile::CheckSandboxSubPath(CtxFile *context, string &resolvedPath)
     string appDir(fullPath);
     appDir = Base::CanonicalizeSpecPath(appDir);
     fullPath = fullPath + Base::GetPathSep() + context->inputLocalPath;
+    // 去除最后的/，保证GetPathWithoutFileName得到的是一个去除了最后一个目录节点的子目录
     while (fullPath.back() == Base::GetPathSep()) {
         fullPath.pop_back();
     }

f5ec6cb5e8a0ad176dced1d64f92b3312cd26ddc
liupeng298 <liupeng298@huawei.com>
2024-12-25 02:03:53 +0000

    update src/host/host_shell_option.cpp.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- src/host/host_shell_option.cpp
+++ src/host/host_shell_option.cpp
@@ -82,7 +82,7 @@ bool ParameterToTlv(char **argv, int argc, TlvBuf &tlvBuf, string &errMsg)
         if (std::strcmp(argv[i], cmdOptionBundle.c_str()) == 0) {
             if (i + 1 >= argc) {
                 WRITE_LOG(LOG_FATAL, "empty bundle name");
-                errMsg = "[E005003] There is no bundle name.";
+                errMsg = "[E003005] There is no bundle name.";
                 break;
             }
             string bundlePath = argv[i + 1];

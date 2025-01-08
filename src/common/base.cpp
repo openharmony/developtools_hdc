8052dcfdff9486f1925dc53064dabcef185cfdfe
kun chen <chenyuankui@huawei.com>
2025-01-08 09:35:07 +0800

    fix sdk fd leak
    Signed-off-by: kun chen <chenyuankui@huawei.com>

--- src/common/base.cpp
+++ src/common/base.cpp
@@ -818,6 +818,7 @@ static void EchoLog(string &buf)
                     break;
                 }
                 if (!uv_run(ptrLoop, UV_RUN_ONCE)) {
+                    uv_loop_close(ptrLoop);
                     ret = true;
                     break;
                 }

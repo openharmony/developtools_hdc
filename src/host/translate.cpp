a92de71c5dd21505c809007ca5ec90bf660b03ea
kun-chen <chenyuankui@huawei.com>
2025-03-21 14:51:52 +0800

    Optimize the description information of OHOS_HDC_HEARTBEAT in hdc -h verbose
    Signed-off-by: kun-chen <chenyuankui@huawei.com>
    Change-Id: I10da8acd277dd573df60d23187795d71aa9eee81

--- src/host/translate.cpp
+++ src/host/translate.cpp
@@ -203,6 +203,8 @@ namespace TranslateCommand {
 #ifdef _WIN32
             "\%OHOS_HDC_SERVER_PORT\%                - Set hdc server listen port(range 1~65535, default 8710)\n"
             "\%OHOS_HDC_LOG_LEVEL\%                  - Set hdc server log level(range 0~5, default 5)\n"
+            "\%OHOS_HDC_HEARTBEAT\%                  - Set whether the hdc heartbeat function is disabled\n"
+            "                                          (1: disabled, other: enabled, default: enabled)\n"
 #ifdef FEATURE_HOST_LOG_COMPRESS
             "\%OHOS_HDC_LOG_LIMIT\%                  - Set the number of logs that trigger the total"
             " log file size statistics (default 300 (files))\n"
@@ -210,6 +212,8 @@ namespace TranslateCommand {
 #else
             "$OHOS_HDC_SERVER_PORT                  - Set hdc server listen port(range 1~65535, default 8710)\n"
             "$OHOS_HDC_LOG_LEVEL                    - Set hdc server log level(range 0~5, default 5)\n"
+            "$OHOS_HDC_HEARTBEAT                    - Set whether the hdc heartbeat function is disabled\n"
+            "                                         (1: disabled, other: enabled, default: enabled)\n"
 #ifdef FEATURE_HOST_LOG_COMPRESS
             "$OHOS_HDC_LOG_LIMIT                    - Set the number of logs that trigger the total"
             " log file size statistics (default 300 (files))\n"

b381e396ea34bba068991407d68f182f834b7299
kun chen <chenyuankui@huawei.com>
2025-03-24 16:06:29 +0800

    add test case for track jpid
    Signed-off-by: kun chen <chenyuankui@huawei.com>

--- test/scripts/testModule/test_hdc_track_jpid.py
+++ test/scripts/testModule/test_hdc_track_jpid.py
@@ -14,22 +14,38 @@
 # limitations under the License.
 import pytest
 from utils import GP, check_app_install, check_app_uninstall, check_cmd_block, \
-    check_hdc_cmd, check_shell, load_gp
+    check_hdc_cmd, check_shell, get_shell_result, load_gp
 
 
 class TestHdcTrackJpid:
-    @pytest.mark.L0
-    @pytest.mark.repeat(2)
-    def test_hdc_jpid(self):
-        assert check_hdc_cmd(f"jpid")
+    bundle_name = "com.example.aacommand07"
+
+    @classmethod
+    def setup_class(self):
+        package_hap = "AACommand07.hap"
+        assert check_app_install(package_hap, self.bundle_name)
+        assert check_shell(f"shell aa start -b {self.bundle_name} -a MainAbility")
+
+    @classmethod
+    def teardown_class(self):
+        assert check_shell(f"shell aa force-stop {self.bundle_name}")
+        assert check_app_uninstall(self.bundle_name)
 
     @pytest.mark.L0
-    @pytest.mark.repeat(2)
+    def test_hdc_jpid(self):
+        pidstr = get_shell_result(f'shell "pidof {self.bundle_name}"')
+        if pidstr:
+            pidstr = pidstr.replace("\r\n", "")
+        assert check_hdc_cmd(f"jpid", pidstr)
+
+    @pytest.mark.L0
     def test_hdc_track_jpid(self):
-        package_hap = "AACommand07.hap"
-        bundle_name = "com.example.aacommand07"
-        assert check_app_install(package_hap, bundle_name)
-        assert check_shell(f"shell aa start -b {bundle_name} -a MainAbility")
-        assert check_cmd_block(f"{GP.hdc_exe} track-jpid -p", f"{bundle_name}", timeout=2)
-        assert check_cmd_block(f"{GP.hdc_exe} track-jpid -a", f"{bundle_name}", timeout=2)
-        assert check_app_uninstall(bundle_name)
+        pidstr = get_shell_result(f'shell "pidof {self.bundle_name}"')
+        if pidstr:
+            pidstr = pidstr.replace("\r\n", "")
+        track_cmd_p = f"{GP.hdc_exe} -t {GP.device_name} track-jpid -p"
+        pattern_p = f"{pidstr} {self.bundle_name}"
+        assert check_cmd_block(track_cmd_p, pattern_p, timeout=2)
+        track_cmd_a = f"{GP.hdc_exe} -t {GP.device_name} track-jpid -a"
+        pattern_a = f"{pidstr} {self.bundle_name} release"
+        assert check_cmd_block(track_cmd_a, pattern_a, timeout=2)

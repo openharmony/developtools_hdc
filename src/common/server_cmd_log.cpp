accc44ce98b50dae3474182761f19638618d666c
luciferWei <weixin100@h-partners.com>
2025-04-27 21:08:54 +0800

    feat:Save the cmdLogs on the hdc server to hdc_cmd.log
    Signed-off-by: luciferWei <weixin100@h-partners.com>

---
+++ src/common/server_cmd_log.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <pthread.h>
+#include "server_cmd_log.h"
+#include "log.h"
+
+namespace Hdc {
+ServerCmdLog& ServerCmdLog::GetInstance()
+{
+    static ServerCmdLog serverCmdLog;
+    return serverCmdLog;
+}
+
+ServerCmdLog::ServerCmdLog()
+{
+    lastFlushTime = std::chrono::system_clock::now();
+    running_ = true;
+}
+
+ServerCmdLog::~ServerCmdLog()
+{
+}
+void ServerCmdLog::PushCmdLogStr(const std::string& cmdLogStr)
+{
+    std::unique_lock<std::mutex> lock(PushCmdLogStrRecordMutex);
+    PushCmdLogStrQueue.push(cmdLogStr);
+}
+
+std::string ServerCmdLog::PopCmdLogStr()
+{
+    std::unique_lock<std::mutex> lock(PushCmdLogStrRecordMutex);
+    if (PushCmdLogStrQueue.empty()) {
+        return "";
+    }
+    std::string cmdLogStr = PushCmdLogStrQueue.front();
+    PushCmdLogStrQueue.pop();
+    lastFlushTime = std::chrono::system_clock::now();
+    return cmdLogStr;
+}
+
+size_t ServerCmdLog::CmdLogStrSize()
+{
+    std::unique_lock<std::mutex> lock(PushCmdLogStrRecordMutex);
+    return PushCmdLogStrQueue.size();
+}
+
+bool ServerCmdLog::GetRunningStatus()
+{
+    return running_;
+}
+
+void ServerCmdLog::SetRunningStatus(bool running)
+{
+    if (running == true) {
+        running_ = true;
+    } else {
+        running_ = false;
+    }
+}
+
+std::chrono::system_clock::time_point ServerCmdLog::GetLastFlushTime()
+{
+    return lastFlushTime;
+}
+
+} // namespace Hdc

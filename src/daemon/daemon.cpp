85b83a90e6ba211cf4620546ccbb200de4f64035
liupeng298 <liupeng298@huawei.com>
2025-08-08 03:27:58 +0000

    update src/daemon/daemon.cpp.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -996,7 +996,7 @@ bool HdcDaemon::FetchCommand(HSession hSession, const uint32_t channelId, const 
         }
         case CMD_KERNEL_CHANNEL_CLOSE: {  // Daemon is only cleaning up the Channel task
             ClearOwnTasks(hSession, channelId);
-            if (*payload != 0) {
+            if (payloadSize >= 1 && *payload != 0) {
                 --(*payload);
                 Send(hSession->sessionId, channelId, CMD_KERNEL_CHANNEL_CLOSE, payload, 1);
             }

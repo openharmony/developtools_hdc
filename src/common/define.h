0aee65a40458a4ece8ed8f51ee5ad7b902d29aaf
liupeng298 <liupeng298@huawei.com>
2025-08-09 09:19:02 +0000

    update src/common/define.h.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- src/common/define.h
+++ src/common/define.h
@@ -124,7 +124,7 @@ constexpr uint16_t NEW_SESSION_DROP_USB_DATA_TIME_MAX_MS = 1000;
 // |----------------------------------------------------------------|
 // | major |reserve| minor |reserve|version|  fix  |   reserve      |
 // |----------------------------------------------------------------|
-// 0x30200000 is 3.2.0a
+// 0x30200100 is 3.2.0b
 constexpr uint32_t HDC_VERSION_NUMBER = 0x30200100;
 constexpr uint32_t HDC_BUF_MAX_BYTES = INT_MAX;
 #ifdef HDC_HOST

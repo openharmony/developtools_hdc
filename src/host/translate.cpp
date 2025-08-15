c978ecc3eb9a852eafe328dc67bc64b600b27ba4
liupeng298 <liupeng298@huawei.com>
2025-08-15 01:22:51 +0000

    update src/host/translate.cpp.
    Signed-off-by: liupeng298 <liupeng298@huawei.com>

--- src/host/translate.cpp
+++ src/host/translate.cpp
@@ -131,6 +131,7 @@ namespace TranslateCommand {
 #else
             " -s [ip:]port                          - Set hdc server listen config\n"
 #endif
+            " -e ip                                 - Specify the local IP address that listens for incoming connections during port forwarding\n"                 
             " -m                                    - Start server at foreground\n"
             " -p                                    - Skip the server startup, run in single client mode\n"
             "\n"

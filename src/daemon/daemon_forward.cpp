89a637ac4023f8abe1f64da60e4b7584431aa998
luciferWei <weixin100@h-partners.com>
2025-09-01 20:36:29 +0800

    Feat：解决异常分支没有关闭fd。 \nSigned-off-by: LuciferWei <weixin100@h-partners.com>
    Signed-off-by: luciferWei <weixin100@h-partners.com>

--- src/daemon/daemon_forward.cpp
+++ src/daemon/daemon_forward.cpp
@@ -54,14 +54,23 @@ bool HdcDaemonForward::SetupJdwpPoint(HCtxForward ctxPoint)
     }
     // do slave connect
     // fd[0] for forward, fd[1] for jdwp
+    // forward to close fd[0], fd[1] for jdwp close
+    int fds[2] = { 0 };
     bool ret = false;
     Base::CreateSocketPair(fds);
+    if (uv_tcp_init(loopTask, &ctxPoint->tcp)) {
+        return ret;
+    }
+    ctxPoint->tcp.data = ctxPoint;
+    if (uv_tcp_open(&ctxPoint->tcp, fds[0])) {
+        Base::CloseSocketPair(fds);
+        return ret;
+    }
     constexpr auto len = sizeof(uint32_t);
     uint8_t flag[1 + len + len];
     flag[0] = SP_JDWP_NEWFD;
     if (memcpy_s(flag + 1, sizeof(flag) - 1, &pid, len) ||
         memcpy_s(flag + 1 + len, sizeof(flag) - len - 1, &fds[1], len)) {
-        Base::CloseSocketPair(fds);
         return ret;
     }
     if (ThreadCtrlCommunicate(flag, sizeof(flag)) > 0) {

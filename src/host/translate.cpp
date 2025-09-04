4cdb6bbe1a318ba395b06e8cf205f31d5d3f35a5
luciferWei <weixin100@h-partners.com>
2025-09-04 17:00:15 +0800

    Feat：同步差异项。 \nSigned-off-by: LuciferWei <weixin100@h-partners.com>
    Signed-off-by: luciferWei <weixin100@h-partners.com>

--- src/host/translate.cpp
+++ src/host/translate.cpp
@@ -91,7 +91,7 @@ namespace TranslateCommand {
               "                                         -a: include debug and release processes\n"
               "                                         -p: don't display debug and release tags\n"
 #ifdef __OHOS__
-              "\n"
+              "\n";
 #else
               "\n"
               "security commands:\n"
@@ -135,7 +135,7 @@ namespace TranslateCommand {
 #else
             " -s [ip:]port                          - Set hdc server listen config\n"
 #endif
-            " -e ip                                 - Set the IP address dor which the host listens during TCP\n"
+            " -e ip                                 - Set the IP address for which the host listens during TCP\n"
             "                                         port forwarding. The default value is 127.0.0.1,\n"
             "                                         cannot run in single client mode\n"
             " -m                                    - Start server at foreground\n"

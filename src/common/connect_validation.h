75c7782495f2157239cca892c63b7d5d78056bfa
maliang_30058418 <18302992148@163.com>
2026-01-26 20:17:10 +0800

    update: 更新文件 connect_validation.h
    Signed-off-by: maliang_30058418 <18302992148@163.com>

--- src/common/connect_validation.h
+++ src/common/connect_validation.h
@@ -12,3 +12,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#ifndef HDC_CONNECT_VALIDATION_H
+#define HDC_CONNECT_VALIDATION_H
+
+#include "auth.h"
+#include "base.h"
+#include "common.h"
+#include "credential_message.h"
+#include "log.h"
+
+
+#include <openssl/evp.h>
+#include <openssl/rsa.h>
+
+namespace HdcValidation {
+
+int GetConnectValidationParam();
+
+#ifdef HDC_HOST
+bool GetPublicKeyHashInfo(string &pubkey_info);
+bool GetPrivateKeyInfo(string &privkey_info);
+bool RsaSignAndBase64(string &buf, Hdc::AuthVerifyType type, string &pemStr);
+
+#else
+
+bool CheckAuthPubKeyIsValid(string &pubkeyHash, string &pubkey);
+#endif
+}  // namespace HdcValidation
+#endif  // HDC_CONNECT_VALIDATION_H

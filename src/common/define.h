de3716965ba7dc2bc564875317501ae88a2e47f1
maliang_30058418 <18302992148@163.com>
2026-05-28 15:25:08 +0800

    update hdc version 3.2.0e
    Signed-off-by: maliang_30058418 <18302992148@163.com>

--- src/common/define.h
+++ src/common/define.h
@@ -123,8 +123,8 @@ constexpr uint16_t NEW_SESSION_DROP_USB_DATA_TIME_MAX_MS = 1000;
 // |----------------------------------------------------------------|
 // | major |reserve| minor |reserve|version|  fix  |   reserve      |
 // |----------------------------------------------------------------|
-// 0x30200300 is 3.2.0d
-constexpr uint32_t HDC_VERSION_NUMBER = 0x30200300;
+// 0x30200300 is 3.2.0e
+constexpr uint32_t HDC_VERSION_NUMBER = 0x30200400;
 constexpr uint32_t HDC_BUF_MAX_BYTES = INT_MAX;
 #ifdef HDC_HOST
 constexpr uint32_t HOST_SOCKETPAIR_SIZE = 3 * 1024 * 1024;

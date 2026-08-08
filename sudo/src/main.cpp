0ca715a750c74adb263322e7b6e5987edc70d43d
jingjing3434 <zhangjingjing99@h-partners.com>
2026-08-08 11:06:15 +0800

    warning
    Signed-off-by: jingjing3434 <zhangjingjing99@h-partners.com>

--- sudo/src/main.cpp
+++ sudo/src/main.cpp
@@ -200,7 +200,7 @@ static void FreeArgvNew(char **argvNew)
  */
 static bool IsElf(const std::string& path)
 {
-    char canonicalPath[PATH_MAX + 1] = {0};
+    char canonicalPath[SUDO_ARG_MAX_NUMS + 1] = {0};
     if (realpath(path.c_str(), canonicalPath) == nullptr) {
         return false;
     }

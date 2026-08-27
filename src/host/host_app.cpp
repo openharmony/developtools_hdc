3ca129899bcf1ac4dadca1fcb5c0be3b0e96dcfa
jingjing3434 <zhangjingjing99@h-partners.com>
2026-08-27 15:47:33 +0800

    add
    Signed-off-by: jingjing3434 <zhangjingjing99@h-partners.com>

--- src/host/host_app.cpp
+++ src/host/host_app.cpp
@@ -161,7 +161,7 @@ void HdcHostApp::CheckMaster(CtxFile *context)
 bool HdcHostApp::CheckInstallContinue(AppModType mode, const char *msg)
 {
     if (msg == nullptr) {
-        LogMsg(MSG_ERROR, "msg is null");
+        LogMsg(LOG_FATAL, "msg is null");
         return false;
     }
     string modeDesc;
